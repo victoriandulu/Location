50e3465022c7000d04ccc0011fb358ca67a2e18c
victoria ndulu <judyvicm1@gmail.com>
2023-02-16 23:08:34 +0300

    initial commit

---
+++ app/src/main/java/com/example/currentlocation/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.currentlocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button showMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showMap = findViewById(R.id.showMap);
+
+        showMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+            }
+        });
+    }
+}
